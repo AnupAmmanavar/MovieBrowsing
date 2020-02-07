7256a382be95239addce8d8d50d879d7fe8a2ca4
AnupPorter <anup.a@theporter.in>
2020-02-07 13:42:42 +0530

    Added views in a carousel

--- app/src/main/java/com/kinley/moviebrowsing/MainActivity.kt
+++ app/src/main/java/com/kinley/moviebrowsing/MainActivity.kt
@@ -4,6 +4,13 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.airbnb.epoxy.CarouselModelBuilder
+import com.airbnb.epoxy.CarouselModel_
+import com.airbnb.epoxy.EpoxyController
+import com.airbnb.epoxy.EpoxyModel
+import com.airbnb.epoxy.carousel
 import com.kinley.moviebrowsing.features.home.HomePageViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -19,12 +26,17 @@ class MainActivity : AppCompatActivity() {
 
     epoxy_rv.withModels {
       val movies = vm.movies.value ?: arrayListOf()
-      movies.forEach { movie ->
-        MovieCellBindingModel_()
-          .id(movie.id)
-          .movie(movie)
-          .addTo(this)
+
+      carousel {
+        id("popular")
+        numViewsToShowOnScreen(2.3f)
+        withModelsFrom(movies) { movie ->
+          MovieCellBindingModel_()
+            .id(movie.id)
+            .movie(movie)
+        }
       }
+
     }
 
     vm.movies.observe(this, Observer {
@@ -32,4 +44,25 @@ class MainActivity : AppCompatActivity() {
     })
 
   }
+}
+
+/** For use in the buildModels method of EpoxyController. A shortcut for creating a Carousel model, initializing it, and adding it to the controller.
+ *
+ */
+inline fun EpoxyController.carousel(modelInitializer: CarouselModelBuilder.() -> Unit) {
+  CarouselModel_().apply {
+    modelInitializer()
+  }.addTo(this)
+}
+
+/** Add models to a CarouselModel_ by transforming a list of items into EpoxyModels.
+ *
+ * @param items The items to transform to models
+ * @param modelBuilder A function that take an item and returns a new EpoxyModel for that item.
+ */
+inline fun <T> CarouselModelBuilder.withModelsFrom(
+  items: List<T>,
+  modelBuilder: (T) -> EpoxyModel<*>
+) {
+  models(items.map { modelBuilder(it) })
 }
--- app/src/main/java/com/kinley/moviebrowsing/extensions/ViewExtensions.kt
+++ app/src/main/java/com/kinley/moviebrowsing/extensions/ViewExtensions.kt
@@ -3,16 +3,17 @@ package com.kinley.moviebrowsing.extensions
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
 import com.kinley.moviebrowsing.R
 
 @BindingAdapter("imageURL")
 fun ImageView.loadImage(imageURL: String?) {
-    val url = if (imageURL != null) "https://image.tmdb.org/t/p/original/$imageURL" else null
+    val url = if (imageURL != null) "https://image.tmdb.org/t/p/w300/$imageURL" else null
     Glide.with(context)
         .load(url)
         .apply(
-            RequestOptions().circleCrop().placeholder(R.drawable.ic_launcher_background)
+            RequestOptions().placeholder(R.drawable.ic_launcher_background)
                 .error(R.drawable.ic_launcher_background)
         )
         .into(this)
