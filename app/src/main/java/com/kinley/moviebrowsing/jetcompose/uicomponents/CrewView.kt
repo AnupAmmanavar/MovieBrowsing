afe20145a8505ea78b11437b19b16ae004067fcc
AnupAmmanavar <ammanavaranup@gmail.com>
2020-02-18 09:05:51 +0530

    created crew horizontal list view

--- app/src/main/java/com/kinley/moviebrowsing/jetcompose/uicomponents/CrewView.kt
+++ app/src/main/java/com/kinley/moviebrowsing/jetcompose/uicomponents/CrewView.kt
@@ -2,12 +2,13 @@ package com.kinley.moviebrowsing.jetcompose.uicomponents
 
 import androidx.compose.Composable
 import androidx.ui.core.Text
+import androidx.ui.foundation.HorizontalScroller
 import androidx.ui.foundation.shape.corner.RoundedCornerShape
 import androidx.ui.graphics.Color
 import androidx.ui.layout.Container
 import androidx.ui.layout.EdgeInsets
 import androidx.ui.layout.LayoutPadding
-import androidx.ui.layout.Padding
+import androidx.ui.layout.Row
 import androidx.ui.material.MaterialTheme
 import androidx.ui.material.surface.Card
 import androidx.ui.text.TextStyle
@@ -15,6 +16,7 @@ import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.dp
 import com.kinley.data.models.Crew
 import com.kinley.moviebrowsing.R
+import com.kinley.moviebrowsing.components.CrewUIComponent
 
 @Composable
 fun CrewView(crew: Crew) {
@@ -22,6 +24,22 @@ fun CrewView(crew: Crew) {
         Card(shape = RoundedCornerShape(10), borderWidth = 8.dp) {
             Text(text = crew.name, style = TextStyle(color = Color(R.color.golden)))
         }
+    }
+}
+
+@Composable
+fun HCrewView(crewListUIComponent: CrewUIComponent) {
+    HorizontalScroller {
+        Row(
+            modifier = LayoutPadding(
+                4.dp
+            )
+        ) {
+            crewListUIComponent.data.forEach { crew ->
+                CrewView(crew = crew)
+            }
+        }
+
     }
 }
 
