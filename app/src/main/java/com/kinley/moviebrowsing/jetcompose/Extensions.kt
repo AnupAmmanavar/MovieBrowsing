ea8c1d376200fd4503dcaa126d274ab265c304e7
AnupPorter <anup.a@theporter.in>
2020-02-21 15:43:54 +0530

    loading image in movie detail view using composable function

--- app/src/main/java/com/kinley/moviebrowsing/jetcompose/Extensions.kt
+++ app/src/main/java/com/kinley/moviebrowsing/jetcompose/Extensions.kt
@@ -2,14 +2,23 @@ package com.kinley.moviebrowsing.jetcompose
 
 
 import androidx.compose.*
+import androidx.core.graphics.drawable.toBitmap
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.ui.foundation.HorizontalScroller
 import androidx.ui.foundation.VerticalScroller
+import androidx.ui.graphics.Image
 import androidx.ui.layout.Column
 import androidx.ui.layout.LayoutPadding
 import androidx.ui.layout.Row
 import androidx.ui.unit.dp
+import coil.Coil
+import coil.api.newGetBuilder
+import coil.request.GetRequest
+import com.kinley.moviebrowsing.extensions.RemoteImage
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Composable
 fun <T> observe(data: LiveData<T>) : T? {
@@ -32,4 +41,39 @@ fun VStack(child: @Composable() () -> Unit) {
 @Composable
 fun HStack(child: @Composable() () -> Unit) {
     HorizontalScroller { Row(modifier = LayoutPadding(4.dp)) { child() } }
+}
+
+/**
+ * Note : This has been picked up from https://tech.instacart.com/exploring-images-in-jetpack-compose-c8ba87089c92
+ */
+
+@Composable
+fun image(data: Any) : Image? {
+    // Positionally memoize the request creation so
+    // it will only be recreated if data changes.
+    val request = remember(data) {
+        Coil.loader().newGetBuilder().data("https://image.tmdb.org/t/p/w300/$data").build()
+    }
+    return image(request)
+}
+
+@Composable
+fun image(request: GetRequest) : Image? {
+    val image = state<Image?> { null }
+
+    // Execute the following code whenever the request changes.
+    onCommit(request) {
+        val job = CoroutineScope(Dispatchers.Main.immediate).launch {
+            // Start loading the image and await the result.
+            val drawable = Coil.loader().get(request)
+            image.value = RemoteImage(drawable.toBitmap())
+        }
+
+        // Cancel the request if the input to onCommit changes or
+        // the Composition is removed from the composition tree.
+        onDispose { job.cancel() }
+    }
+
+    // Emit a null Image to start with.
+    return image.value
 }
--- app/src/main/java/com/kinley/moviebrowsing/jetcompose/uicomponents/MovieDetailView.kt
+++ app/src/main/java/com/kinley/moviebrowsing/jetcompose/uicomponents/MovieDetailView.kt
@@ -1,7 +1,6 @@
 package com.kinley.moviebrowsing.jetcompose.uicomponents
 
 import androidx.compose.Composable
-import androidx.core.graphics.drawable.toBitmap
 import androidx.ui.core.Text
 import androidx.ui.foundation.DrawImage
 import androidx.ui.layout.Column
@@ -10,40 +9,31 @@ import androidx.ui.layout.LayoutPadding
 import androidx.ui.layout.Row
 import androidx.ui.material.Divider
 import androidx.ui.material.MaterialTheme
+import androidx.ui.res.imageResource
 import androidx.ui.text.style.TextOverflow
 import androidx.ui.unit.dp
 import com.kinley.data.models.Movie
+import com.kinley.moviebrowsing.R
 import com.kinley.moviebrowsing.components.MovieDelegate
-import com.kinley.moviebrowsing.extensions.ImageState
-import com.kinley.moviebrowsing.extensions.RemoteImage
-import com.kinley.moviebrowsing.extensions.loadImage
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
+import com.kinley.moviebrowsing.jetcompose.image
 
 @Composable
 fun MovieDetailView(movie: Movie, delegate: MovieDelegate) {
 
-    val state = ImageState()
+  val image = image(data = movie.poster_path ?: "")
+    ?: imageResource(id = R.drawable.image_missing_icon)
 
-    GlobalScope.launch {
-        val drawable = loadImage(movie.poster_path ?: "")
-        MainScope().launch {
-            state.image = RemoteImage(drawable.toBitmap())
-        }
+
+  val typography = MaterialTheme.typography()
+  Row(modifier = LayoutPadding(8.dp)) {
+    Container(width = 160.dp, height = 160.dp) {
+      DrawImage(image = image)
     }
 
-
-    val typography = MaterialTheme.typography()
-    Row(modifier = LayoutPadding(8.dp)) {
-        Container(width = 160.dp, height = 160.dp) {
-            DrawImage(image = state.image)
-        }
-
-        Column {
-            Text(movie.title, style = typography.h6)
-            Divider()
-            Text(movie.overview ?: "", maxLines = 5, overflow = TextOverflow.Ellipsis)
-        }
+    Column {
+      Text(movie.title, style = typography.h6)
+      Divider()
+      Text(movie.overview ?: "", maxLines = 5, overflow = TextOverflow.Ellipsis)
     }
+  }
 }
